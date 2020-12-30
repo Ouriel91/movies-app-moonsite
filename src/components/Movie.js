20ef3221199f7c605e36037dc65628b0be55429b
Ouriel91 <orieloh91@gmail.com>
2020-12-30 15:33:37 +0200

    Added basic style to movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,16 +2,22 @@ import React from 'react'
 import './Movie.css'
 
 function Movie(props) {
-    const {title,poster_path,overview,vote_average,id} = props.movie //object of selected movie
-    const {image_api, favMovie} = props //other props that passed to this component
+    const { title, poster_path, overview, vote_average, id } = props.movie //object of selected movie
+    const { image_api, favMovie } = props //other props that passed to this component
 
     return (
         <div className="movie_container">
-            <h1>{title}</h1>
-            <img style={{height:'40vh', width:'50vw'}} alt="movie poster" src={image_api+poster_path} />
-            <p>{overview}</p>
-            <p><b>{vote_average}</b></p>
-            <button onClick={() => favMovie(id)}>+</button>
+            <img className="movie_poster" alt="movie poster" src={image_api + poster_path} />
+            <div className="movie_details">
+                <div className="details_container">
+                    <h1>{title}</h1>
+                    <p style={{alignItems:'flex-end'}}><b>{vote_average}</b></p>
+                </div>
+                <div className="details_container">
+                    <p>{overview}</p>
+                    <button onClick={() => favMovie(id)}>+</button>
+                </div>
+            </div>
         </div>
     )
 }
